b709119457a379490e2f48658a9fee136308f32e
George <ruiz.fullstack@gmail.com>
2020-10-18 21:22:42 -0400

    push to net

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,15 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-        {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `roboto mono`,
-        ],
-        display: 'swap'
-      }
-    }
+ 
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
