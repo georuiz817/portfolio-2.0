389a7ed5afa2c5c4a9cf82b2347c4b9eaf4dba87
George <ruiz.fullstack@gmail.com>
2020-10-26 20:29:21 -0400

    tempo add

--- src/components/project-loop.js
+++ src/components/project-loop.js
@@ -3,6 +3,14 @@ import React from "react"
 const ProjectLoop = () => {
   let projectArray = [
     {
+      name: "Temp(o)",
+      icon: "🌧️",
+      description:
+        "Get todays temp around the world and some nice tunes while you're at it.",
+      site: "temp-o.netlify.app/#/",
+      gitHub: "https://github.com/georuiz817/temp-o",
+    },
+    {
       name: "Horizons-Library",
       icon: "🍃",
       description:
@@ -17,26 +25,31 @@ const ProjectLoop = () => {
         "Celebrate the holiday by contributing to a sponsored charitable cause. Interact and discover what Thanksgiving is all about.",
       gitHub: "https://github.com/georuiz817/Gobbler",
     },
-
-    {
-      name: "Turtles-App",
-      icon: "🍕",
-      description:
-        "Who doesn't love pizza? Gain inspiration by viewing the board and feed a turtle your favorite classic (or obscure) pizza slices.",
-      gitHub: "https://github.com/georuiz817/turtles-app-",
-    },
   ]
   return projectArray.map(p => (
     <div className="p-info">
-       <h1>
+      <h1>
         {p.name}
         {p.icon}
       </h1>
       <p>
         <em>{p.description}</em>
       </p>
-      <p>- GitHub: <a target="_blank" rel="noopener noreferrer" href={p.gitHub}>{p.gitHub}</a></p>
-      {p.site ? <p> - live site: <a target="_blank" rel="noopener noreferrer" href={p.site}>{p.site}</a></p> : null}
+      <p>
+        - GitHub:{" "}
+        <a target="_blank" rel="noopener noreferrer" href={p.gitHub}>
+          {p.gitHub}
+        </a>
+      </p>
+      {p.site ? (
+        <p>
+          {" "}
+          - live site:{" "}
+          <a target="_blank" rel="noopener noreferrer" href={p.site}>
+            {p.site}
+          </a>
+        </p>
+      ) : null}
       <hr></hr>
     </div>
   ))
